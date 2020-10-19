9606468e5778b93f753deb2c87b8e4aa2222ce9f
Absolitudo <absolitudo@gmail.com>
2020-10-20 00:47:09 +0200

    updateoldlogdata

--- updateOldLogData.js
+++ updateOldLogData.js
@@ -3,15 +3,18 @@ const fs = require("fs");
 const { getLogs } = require("./helpers");
 const oldData = require("./logData.json");
 
-(async function() {
+(async function () {
     if (!oldData) {
         console.log("No previous log data have been found");
-        return 0;
-    }
-    let data = { ...JSON.parse(JSON.stringify(oldData)) };
-    let newData = await getLogs(oldData.lastLogIds);
-    data.lastLogIds = newData.lastLogIds;
+        let newData = await getLogs();
+        fs.writeFileSync("newLogData.json", JSON.stringify(newData));
+    } else {
+        let data = { ...JSON.parse(JSON.stringify(oldData)) };
+        let newData = await getLogs(oldData.lastLogIds);
+        data.lastLogIds = newData.lastLogIds;
 
-    data.logs = data.logs.concat(newData.logs);
-    fs.writeFileSync("newLogData.json", JSON.stringify(data));
+        data.logs = data.logs.concat(newData.logs);
+        fs.writeFileSync("newLogData.json", JSON.stringify(data));
+    }
+    console.log("done");
 })();
