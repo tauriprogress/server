672e270842cc6945ca94515e61283808b6d65a1c
Absolitudo <absolitudo@gmail.com>
2020-11-21 07:55:06 +0100

    start redis server if not dev env

--- start.js
+++ start.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
+const exec = util.promisify(require("child_process").exec);
 const port = process.env.PORT || 3001;
+const dev = process.env.DEV_ENV;
 const app = require("./src/server");
+
+if (!dev) {
+    exec("./redis-stable/src/redis-server ./redis.conf");
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
