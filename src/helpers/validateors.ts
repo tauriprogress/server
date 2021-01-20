98bdcb5bfbf38065350224b7d586f8238611ddb8
Absolitudo <absolitudo@gmail.com>
2021-01-20 21:28:46 +0100

    helper function validators

---
+++ src/helpers/validateors.ts
@@ -0,0 +1,18 @@
+import { environment } from "../environment";
+
+export function validRaidId(raidId: number) {
+    for (const raid of environment.raids) {
+        if (raid.id === raidId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+export function validRealm(realm: string) {
+    return environment.realms.hasOwnProperty(realm);
+}
+
+export function validClass(characterClass: number) {
+    return environment.characterClassNames.hasOwnProperty(characterClass);
+}
