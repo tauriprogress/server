6b1a07679d8f1a9946b31797837e6e4eea58a07f
Absolitudo <absolitudo@gmail.com>
2021-01-20 15:58:37 +0100

    check if realm group env var is valid

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,15 +1,34 @@
 import * as constants from "tauriprogress-constants";
 
+const realmGroups = {
+    tauri: "",
+    crystalsong: ""
+};
+
+type realmGroupType = keyof typeof realmGroups;
+
+function isRealmGroup(x: string): x is realmGroupType {
+    return realmGroups.hasOwnProperty(x);
+}
+
 class Environment {
-    readonly REALM_GROUP: string | undefined;
-    readonly TAURI_API_KEY: string | undefined;
-    readonly TAURI_API_SECRET: string | undefined;
-    readonly MONGODB_USER: string | undefined;
-    readonly MONGODB_PASSWORD: string | undefined;
-    readonly MONGODB_ADDRESS: string | undefined;
+    readonly REALM_GROUP: realmGroupType;
+    readonly TAURI_API_KEY: string;
+    readonly TAURI_API_SECRET: string;
+    readonly MONGODB_USER: string;
+    readonly MONGODB_PASSWORD: string;
+    readonly MONGODB_ADDRESS: string;
 
     constructor() {
-        this.REALM_GROUP = process.env.REALM_GROUP;
+        if (process.env.REALM_GROUP && isRealmGroup(process.env.REALM_GROUP)) {
+            this.REALM_GROUP = process.env.REALM_GROUP;
+        } else {
+            console.error(
+                `Environment variable REALM_GROUP=${process.env.REALM_GROUP} is invalid.`
+            );
+            process.exit(0);
+        }
+
         this.TAURI_API_KEY = process.env.REALM_GROUP;
         this.TAURI_API_SECRET = process.env.REALM_GROUP;
         this.MONGODB_USER = process.env.REALM_GROUP;
