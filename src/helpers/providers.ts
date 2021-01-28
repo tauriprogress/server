519e6c83c5054d07c0c0e4dd563c2950d728b6df
akoskoc <akoskoc@gmail.com>
2021-01-28 12:03:39 +0100

    date related helpers

--- src/helpers/providers.ts
+++ src/helpers/providers.ts
@@ -25,3 +25,8 @@ export function getLatestWednesday(currentDate: Date = new Date()) {
         10
     );
 }
+
+export function unshiftDateDay(day: number) {
+    // date.getDay returns a number that represents the day, 0 by deafult is sunday, instead i prefer 0 to be monday, thats the reason for this function
+    return day - 1 >= 0 ? day - 1 : 6;
+}
