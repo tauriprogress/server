bcdb6af4b0cefe852b7143d84f7620aeace79223
Absolitudo <absolitudo@gmail.com>
2021-01-28 11:52:33 +0100

    getlatestwednesday

--- src/helpers/providers.ts
+++ src/helpers/providers.ts
@@ -7,3 +7,21 @@ export function createCharacterId(
 ) {
     return `${name},${environment.shortRealms[realm]},${spec}`;
 }
+
+export function getLatestWednesday(currentDate: Date = new Date()) {
+    const currentDay = currentDate.getDay();
+
+    const wednesdayDaysAgo = (currentDay < 3 ? currentDay + 7 : currentDay) - 3;
+
+    let lastWednesdayDate = currentDate.getDate() - wednesdayDaysAgo;
+    if (currentDay === 3 && currentDate.getHours() < 9) {
+        lastWednesdayDate -= 7;
+    }
+
+    return new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        lastWednesdayDate,
+        10
+    );
+}
