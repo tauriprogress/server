074c40a01efed4f02d3e5f64d5a981258093af52
Absolitudo <absolitudo@gmail.com>
2021-01-29 13:07:22 +0100

    getlogfaction

--- src/helpers/providers.ts
+++ src/helpers/providers.ts
@@ -1,4 +1,5 @@
 import { environment } from "../environment";
+import { RaidLogWithRealm } from "../types";
 
 export function createCharacterId(
     name: string,
@@ -29,4 +30,35 @@ export function getLatestWednesday(currentDate: Date = new Date()) {
 export function unshiftDateDay(day: number) {
     // date.getDay returns a number that represents the day, 0 by deafult is sunday, instead i prefer 0 to be monday, thats the reason for this function
     return day - 1 >= 0 ? day - 1 : 6;
+}
+
+export function getLogFaction(log: RaidLogWithRealm) {
+    /**
+     * The reasons for this function:
+     *  - There is no reference to the faction the kill took place in
+     *  - The only way to determine faction is to look at guild or character data
+     *  - The guild/character data is unreliable to determine faction
+     *      because it is picked from a relational table at the time of the request based on a guild/char id,
+     *      this means that the guild/char data may change over time
+     *      (eg: guild/char gone through a faction change)
+     *
+     * To determine faction with higher probability,
+     *  this function counts the faction each char belongs to and picks the highest
+     *
+     */
+
+    let alliance = 0;
+    let horde = 0;
+    for (let member of log.members) {
+        const race = String(
+            member.race
+        ) as keyof typeof environment.characterRaceToFaction;
+        if (environment.characterRaceToFaction[race] === 0) {
+            alliance++;
+        } else {
+            horde++;
+        }
+    }
+
+    return horde > alliance ? 1 : 0;
 }
