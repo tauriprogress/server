b69147895876d6b24455ad9890ee35fe710aca66
Absolitudo <absolitudo@gmail.com>
2021-02-07 19:15:45 +0100

    default raidboss

---
+++ src/helpers/raidBoss/index.ts
@@ -0,0 +1,18 @@
+import { RaidBoss } from "../../types";
+
+export function getDefaultBoss(): RaidBoss {
+    return {
+        _id: "",
+        raidId: 0,
+        name: "",
+        difficulty: 0,
+        killCount: 0,
+        recentKills: [],
+        fastestKills: {},
+        firstKills: {},
+        bestDps: {},
+        bestHps: {},
+        bestDpsNoCat: undefined,
+        bestHpsNoCat: undefined
+    };
+}
