72c561cba97152d6448204454f8c62e5d97d4d22
Absolitudo <absolitudo@gmail.com>
2021-02-08 10:45:56 +0100

    cache

---
+++ src/database/cache.ts
@@ -0,0 +1,40 @@
+import * as NodeCache from "node-cache";
+
+class Cache {
+    public guildList: NodeCache;
+    public raidSummary: NodeCache;
+    public character: NodeCache;
+    public raidBoss: NodeCache;
+    public leaderboard: NodeCache;
+
+    constructor() {
+        this.guildList = new NodeCache({
+            stdTTL: 20 * 60,
+            checkperiod: 60,
+            useClones: false
+        });
+        this.raidSummary = new NodeCache({
+            stdTTL: 20 * 60,
+            checkperiod: 60,
+            useClones: false
+        });
+        this.character = new NodeCache({
+            stdTTL: 4 * 60,
+            checkperiod: 60,
+            useClones: false,
+            maxKeys: 150
+        });
+        this.raidBoss = new NodeCache({
+            stdTTL: 0,
+            useClones: false
+        });
+        this.leaderboard = new NodeCache({
+            stdTTL: 0,
+            useClones: false
+        });
+    }
+}
+
+const cache = new Cache();
+
+export default cache;
