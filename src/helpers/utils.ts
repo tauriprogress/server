4bdc40f6c2600bbd1224d2aa45d6e20ca4042f22
akoskoc <akoskoc@gmail.com>
2021-11-09 06:56:35 +0100

    ensure file utility function

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as path from "path";
+
 export function runGC() {
     try {
         if (global.gc) {
@@ -9,9 +12,19 @@ export function runGC() {
 }
 
 export function sleep(ms: number): Promise<true> {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve(true);
         }, ms);
     });
+}
+
+export function ensureFile(filePath: string) {
+    if (!fs.existsSync(filePath)) {
+        const dirname = path.dirname(filePath);
+        if (dirname && !fs.existsSync(dirname)) {
+            fs.mkdirSync(dirname);
+        }
+        fs.closeSync(fs.openSync(filePath, "w"));
+    }
 }
