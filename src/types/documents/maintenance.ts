8ad74dc1b94f5e8d8fb99fae621879c7e2d8475d
akoskoc <akoskoc@gmail.com>
2022-01-08 18:15:06 +0100

    maintenance document

---
+++ src/types/documents/maintenance.ts
@@ -0,0 +1,12 @@
+import { ObjectId } from "mongodb";
+import { Realm } from "..";
+
+export interface MaintenanceDocument extends Document {
+    _id: ObjectId;
+    lastUpdated: number;
+    lastGuildsUpdate: number;
+    lastLogIds: {
+        [key in Realm]?: number;
+    };
+    isInitalized: boolean;
+}
