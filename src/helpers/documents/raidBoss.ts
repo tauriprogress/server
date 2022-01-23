3487614510a9241265d095d946c3c1bb73246acb
akoskoc <akoskoc@gmail.com>
2022-01-23 19:24:46 +0100

    add log to raidboss doc

--- src/helpers/documents/raidBoss.ts
+++ src/helpers/documents/raidBoss.ts
@@ -1,14 +1,22 @@
-import { Difficulty, RaidBossDocument, RaidId } from "../../types";
-import { raidBossId } from "../../helpers";
+import {
+    Difficulty,
+    RaidBossDocument,
+    RaidId,
+    Realm,
+    TrimmedLog,
+    Faction,
+} from "../../types";
+import { raidBossId } from "../ids";
+import { getNestedObjectValue, addNestedObjectValue } from "..";
 
 export function createRaidBossDocument(
     raidId: RaidId,
-    bossId: number,
+    bossId: ReturnType<typeof raidBossId>,
     bossName: string,
     difficulty: Difficulty
 ): RaidBossDocument {
     return {
-        _id: raidBossId(bossId, difficulty),
+        _id: bossId,
         raidId: raidId,
         name: bossName,
         difficulty: difficulty,
@@ -21,4 +29,70 @@ export function createRaidBossDocument(
         bestDpsNoCat: undefined,
         bestHpsNoCat: undefined,
     };
+}
+
+export function addLogToRaidBossDocument(
+    raidBossDocument: RaidBossDocument,
+    trimmedLog: TrimmedLog,
+    realm: Realm,
+    faction: Faction
+) {
+    raidBossDocument.killCount += 1;
+
+    raidBossDocument.recentKills.unshift(trimmedLog);
+    raidBossDocument.recentKills.splice(50);
+
+    const logCategorization = [realm, faction];
+
+    const fastestExists = !!getNestedObjectValue(
+        raidBossDocument.fastestKills,
+        logCategorization
+    );
+
+    if (!fastestExists) {
+        raidBossDocument.fastestKills = addNestedObjectValue(
+            raidBossDocument.fastestKills,
+            logCategorization,
+            [trimmedLog]
+        );
+    } else {
+        const arr = raidBossDocument.fastestKills[realm]![faction];
+        for (let i = 0; i < arr.length; i++) {
+            if (trimmedLog.fightLength < arr[i].fightLength) {
+                raidBossDocument.fastestKills[realm]![faction].splice(
+                    i,
+                    0,
+                    trimmedLog
+                );
+                raidBossDocument.fastestKills[realm]![faction].splice(50);
+            }
+        }
+    }
+
+    const firstExists = !!getNestedObjectValue(
+        raidBossDocument.firstKills,
+        logCategorization
+    );
+
+    if (!firstExists) {
+        raidBossDocument.firstKills = addNestedObjectValue(
+            raidBossDocument.firstKills,
+            logCategorization,
+            [trimmedLog]
+        );
+    } else {
+        const arr = raidBossDocument.firstKills[realm]![faction];
+        for (let i = 0; i < arr.length; i++) {
+            if (trimmedLog.date < arr[i].date) {
+                raidBossDocument.firstKills[realm]![faction].splice(
+                    i,
+                    0,
+                    trimmedLog
+                );
+                raidBossDocument.firstKills[realm]![faction].splice(3);
+            }
+        }
+    }
+
+    return raidBossDocument;
 }
