fcb778297deb0202b4ebc4e22d8af5709a4df7fd
akoskoc <akoskoc@gmail.com>
2022-02-17 11:21:59 +0100

    create maintenance document

---
+++ src/helpers/documents/maintenance.ts
@@ -0,0 +1,12 @@
+import { MaintenanceDocument } from "../../types";
+import { ObjectId } from "mongodb";
+
+export function createMaintenanceDocument(): MaintenanceDocument {
+    return {
+        _id: new ObjectId(),
+        lastUpdated: 0,
+        lastGuildsUpdate: 0,
+        lastLogIds: {},
+        isInitalized: true,
+    };
+}
--- src/types/documents/maintenance.ts
+++ src/types/documents/maintenance.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from "mongodb";
+import { ObjectId, Document } from "mongodb";
 import { Realm } from "..";
 
 export interface MaintenanceDocument extends Document {
