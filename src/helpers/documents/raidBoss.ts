11f787b779e1c9c82e390fc63c3507a951f45b6e
akoskoc <akoskoc@gmail.com>
2022-03-03 16:21:02 +0100

    update raidboss document

--- src/helpers/documents/raidBoss.ts
+++ src/helpers/documents/raidBoss.ts
@@ -1,6 +1,7 @@
 import {
     Difficulty,
     RaidBossDocument,
+    LooseObject,
     RaidId,
     Realm,
     TrimmedLog,
@@ -8,8 +9,14 @@ import {
     CharacterDocument,
     CombatMetric,
 } from "../../types";
-import { raidBossId } from "../ids";
-import { getNestedObjectValue, addNestedObjectValue } from "..";
+import {
+    getNestedObjectValue,
+    addNestedObjectValue,
+    raidBossId,
+    capitalize,
+} from "..";
+import environment from "../../environment";
+import { combatMetrics, factions } from "../../constants";
 
 export function createRaidBossDocument(
     raidId: RaidId,
@@ -178,4 +185,155 @@ export function addCharacterDocumentToRaidBossDocument(
         );
     }
     return raidBossDocument;
+}
+
+export function updateRaidBossDocument(
+    oldBoss: RaidBossDocument,
+    boss: RaidBossDocument
+) {
+    let updatedRaidBoss: RaidBossDocument = {
+        ...JSON.parse(JSON.stringify(oldBoss)),
+        latestKills: boss.latestKills.concat(oldBoss.recentKills).splice(50),
+        killCount: oldBoss.killCount + boss.killCount,
+    };
+
+    for (const combatMetric of combatMetrics) {
+        const bestOfNoCatKey = `best${capitalize(combatMetric)}NoCat` as const;
+
+        const oldBestChar = oldBoss[bestOfNoCatKey];
+        const newBestChar = boss[bestOfNoCatKey];
+
+        if (!oldBestChar) {
+            updatedRaidBoss[bestOfNoCatKey] = newBestChar;
+        } else if (newBestChar) {
+            const oldBest = oldBestChar[combatMetric];
+            const newBest = newBestChar[combatMetric];
+
+            if (oldBest && newBest && newBest > oldBest) {
+                updatedRaidBoss[bestOfNoCatKey] = newBestChar;
+            }
+        }
+    }
+
+    for (const realm of environment.realms) {
+        for (const faction of factions) {
+            const fastestKills = boss.fastestKills?.[realm]?.[faction];
+            if (fastestKills) {
+                let updatedFastestKills = fastestKills;
+                const oldFastestKills =
+                    oldBoss.fastestKills?.[realm]?.[faction];
+
+                if (oldFastestKills) {
+                    updatedFastestKills = fastestKills.concat(oldFastestKills);
+                }
+
+                updatedRaidBoss.fastestKills = addNestedObjectValue(
+                    updatedRaidBoss.fastestKills,
+                    [realm, faction],
+                    updatedFastestKills
+                        .sort((a, b) => a.fightLength - b.fightLength)
+                        .splice(50)
+                );
+            }
+
+            const firstKills = boss.firstKills?.[realm]?.[faction];
+            if (firstKills) {
+                let updatedFirstKills = firstKills;
+                const oldFirstKills = oldBoss.firstKills?.[realm]?.[faction];
+
+                if (oldFirstKills) {
+                    updatedFirstKills = firstKills.concat(oldFirstKills);
+                }
+
+                updatedRaidBoss.firstKills = addNestedObjectValue(
+                    updatedRaidBoss.firstKills,
+                    [realm, faction],
+                    updatedFirstKills.sort((a, b) => a.date - b.date).splice(3)
+                );
+            }
+
+            for (const key in environment.characterClassSpecs) {
+                const classId =
+                    key as unknown as keyof typeof environment.characterClassNames;
+
+                for (const key in environment.characterClassSpecs[classId]) {
+                    const specId =
+                        key as unknown as typeof environment.characterClassSpecs[typeof classId][number];
+
+                    for (const combatMetric of combatMetrics) {
+                        const bestOfKey = `best${capitalize(
+                            combatMetric
+                        )}` as const;
+
+                        const categorization = [
+                            bestOfKey,
+                            realm,
+                            faction,
+                            classId,
+                            specId,
+                        ];
+
+                        let oldChars =
+                            oldBoss[bestOfKey]?.[realm]?.[faction]?.[classId]?.[
+                                specId
+                            ] || [];
+
+                        let newChars =
+                            boss[bestOfKey]?.[realm]?.[faction]?.[classId]?.[
+                                specId
+                            ] || [];
+
+                        const bestCharacters: LooseObject = {};
+
+                        for (const bestCharacter of [
+                            ...oldChars,
+                            ...newChars,
+                        ]) {
+                            const currentPerformance =
+                                bestCharacter[combatMetric];
+                            if (
+                                !bestCharacters[bestCharacter._id] ||
+                                (bestCharacters[bestCharacter._id] &&
+                                    currentPerformance &&
+                                    currentPerformance >
+                                        bestCharacters[bestCharacter._id][
+                                            combatMetric
+                                        ])
+                            ) {
+                                bestCharacters[bestCharacter._id] =
+                                    bestCharacter;
+                            }
+                        }
+
+                        const updatedBestsOfCombatMetric: CharacterDocument[] =
+                            [];
+
+                        for (const charId in bestCharacters) {
+                            updatedBestsOfCombatMetric.push(
+                                bestCharacters[charId]
+                            );
+                        }
+
+                        updatedRaidBoss = addNestedObjectValue(
+                            updatedRaidBoss,
+                            categorization,
+                            updatedBestsOfCombatMetric
+                                .sort((a, b) => {
+                                    const bPerf = b[combatMetric];
+                                    const aPerf = a[combatMetric];
+                                    if (bPerf && aPerf) {
+                                        return bPerf - aPerf;
+                                    } else {
+                                        return 0;
+                                    }
+                                })
+                                .slice(0, 10)
+                        ) as RaidBossDocument;
+                    }
+                }
+            }
+        }
+    }
+
+    return updatedRaidBoss;
 }
