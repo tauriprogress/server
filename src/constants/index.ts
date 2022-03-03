7e7174263fa9d922d7388a44f1ff9574bdcbe422
akoskoc <akoskoc@gmail.com>
2022-03-03 16:20:21 +0100

    more constant vars

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const week = 1000 * 60 * 60 * 24 * 7;
 export const pathToLastLogIds = "./logs/lastLogIds.json";
 export const pathToLogs = "./logs/logs.txt";
+export const factions = [0, 1] as const;
+export const combatMetrics = ["dps", "hps"] as const;
