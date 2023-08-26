5f05ca3f2d7f5635b49f2ed02ea42b10cd71a076
akoskoc <akoskoc@gmail.com>
2023-08-26 09:54:40 +0200

    db connection

---
+++ src/database/DBConnection.ts
@@ -0,0 +1,41 @@
+import { Db, MongoClient } from "mongodb";
+import environment from "../environment";
+import { ERR_DB_CONNECTION } from "../helpers/errors";
+
+class DBConnection {
+    private connection: Db | undefined;
+    private client: MongoClient | undefined;
+
+    public async connect() {
+        try {
+            console.log("Connecting to database");
+            const client = new MongoClient(
+                `mongodb+srv://${environment.MONGODB_USER}:${environment.MONGODB_PASSWORD}@${environment.MONGODB_ADDRESS}`
+            );
+            await client.connect();
+
+            this.client = client;
+            this.connection = this.client.db("tauriprogress");
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    public getConnection() {
+        if (this.connection) {
+            return this.connection;
+        }
+
+        throw ERR_DB_CONNECTION;
+    }
+
+    public async disconnect() {
+        await this.client?.close();
+        this.connection = undefined;
+        this.client = undefined;
+    }
+}
+
+const dbConnection = new DBConnection();
+
+export default dbConnection;
