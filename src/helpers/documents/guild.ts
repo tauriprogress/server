fed6339af1fcd0afe0f70eb17f93658407c4a964
akoskoc <akoskoc@gmail.com>
2023-09-07 06:52:02 +0200

    guild doc getdoc & addlog

--- src/helpers/documents/guild.ts
+++ src/helpers/documents/guild.ts
@@ -1,32 +1,396 @@
-import environment from "../../environment";
 import {
-    getGuildId,
-    getNestedObjectValue,
+    WeekId,
     addNestedObjectValue,
-    uniqueLogs,
+    getNestedObjectValue,
     getRaidInfoFromName,
-    timeTransformer,
-    logs,
+    id,
+    time,
+    uniqueLogs,
 } from "..";
-import {
-    GuildDocument,
-    GuildRaidDays,
-    Realm,
-    Faction,
-    GuildBoss,
-    Difficulty,
-    RaidName,
-    GuildRankingFull,
-    RaidLogWithRealm,
-    GuildKillLog,
-    GuildLatestKill,
-    GuildCompletion,
-    GuildRaids,
-    GuildRankingFastest,
-    GuildMember,
-} from "../../types";
-import { week } from "../../constants";
+import environment from "../../environment";
 import tauriApi from "../../tauriApi";
+import {
+    ClassId,
+    Difficulty,
+    Faction,
+    MilliSecond,
+    RaidLogWithRealm,
+    RaidName,
+    Realm,
+    Second,
+} from "../../types";
+
+import { Document } from "mongodb";
+
+import { log as logHelper } from "../log";
+
+export interface GuildDocument extends Document {
+    _id: ReturnType<typeof id.guildId>;
+    f: Faction;
+    realm: Realm;
+    name: string;
+    members: GuildMember[];
+    ranks: string[];
+    activity: GuildActivity;
+    progression: GuildProgression;
+    raidDays: GuildRaidDays;
+    ranking: Ranking;
+}
+
+export interface GuildMember {
+    name: string;
+    class: ClassId;
+    rankName: string;
+    lvl: number;
+    race: string;
+}
+
+export type GuildActivity = {
+    [key in Difficulty]?: number;
+};
+
+export interface GuildProgression {
+    latestKills: GuildLatestKill[];
+    completion: GuildCompletion;
+    raids: GuildRaids;
+}
+
+export interface GuildLatestKill {
+    id: number;
+    date: number;
+    boss: string;
+    difficulty: Difficulty;
+}
+
+export interface GuildRaidDays {
+    total: number[][];
+    latest: number[][];
+}
+
+export interface GuildCompletion {
+    completed: false | number;
+    bossesDefeated: number;
+    difficulties: {
+        [propName: number]: {
+            completed: false | number;
+            bossesDefeated: number;
+        };
+    };
+}
+
+export type GuildRaids = {
+    [key in RaidName]?: {
+        [key in Difficulty]?: {
+            [propName: string]: GuildBoss;
+        };
+    };
+};
+
+export interface GuildBoss {
+    killCount: number;
+    firstKills: GuildKillLog[];
+    fastestKills: GuildKillLog[];
+    latestKills: GuildKillLog[];
+}
+
+export interface GuildKillLog {
+    id: number;
+    fightLength: MilliSecond;
+    date: Second;
+}
+
+type Ranking = {
+    [key in RaidName]?: {
+        [key in Difficulty]?: {
+            fullClear: GuildRankingFull;
+            fastestKills: GuildRankingFastest;
+        };
+    };
+};
+
+export interface GuildRankingFull {
+    time: number | false;
+    logs: GuildRankingLog[];
+    weeks: {
+        [propName: WeekId]: GuildRankingRaidGroup[];
+    };
+}
+
+export interface GuildRankingFastest {
+    time: number | false;
+    logs: GuildRankingLog[];
+}
+
+export interface GuildRankingLog {
+    id: number;
+    date: number;
+    fightLength: number;
+    bossName: string;
+}
+
+export interface GuildRankingRaidGroup {
+    members: string[];
+    logs: GuildRankingLog[];
+}
+
+export type GuildLeaderboard = {
+    name: string;
+    f: Faction;
+    realm: Realm;
+    ranking: Ranking;
+}[];
+
+export class GuildDocumentController {
+    _id: ReturnType<typeof id.guildId>;
+    f: Faction;
+    realm: Realm;
+    name: string;
+    members: GuildMember[];
+    ranks: string[];
+    activity: GuildActivity;
+    progression: GuildProgression;
+    raidDays: GuildRaidDays;
+    ranking: Ranking;
+
+    constructor(
+        obj:
+            | {
+                  guildName: string;
+                  realm: Realm;
+                  faction: Faction;
+              }
+            | GuildDocument
+    ) {
+        if (this.isGuildDocument(obj)) {
+            obj = JSON.parse(JSON.stringify(obj)) as GuildDocument;
+
+            this._id = obj._id;
+            this.f = obj.faction;
+            this.realm = obj.realm;
+            this.name = obj.guildName;
+            this.members = obj.members;
+            this.ranks = obj.ranks;
+            this.activity = obj.activity;
+            this.progression = obj.progression;
+            this.raidDays = obj.raidDays;
+            this.ranking = obj.ranking;
+        } else {
+            this._id = id.guildId(obj.guildName, obj.realm);
+            this.f = obj.faction;
+            this.realm = obj.realm;
+            this.name = obj.guildName;
+            this.members = [];
+            this.ranks = [];
+            this.activity = {};
+            this.progression = {
+                latestKills: [],
+                completion: {
+                    completed: false,
+                    bossesDefeated: 0,
+                    difficulties: {},
+                },
+                raids: {},
+            };
+            this.raidDays = this.createGuildRaidDays();
+            this.ranking = {};
+        }
+    }
+
+    getDocument(): GuildDocument {
+        return {
+            _id: this._id,
+            f: this.f,
+            realm: this.realm,
+            name: this.name,
+            members: this.members,
+            ranks: this.ranks,
+            activity: this.activity,
+            progression: this.progression,
+            raidDays: this.raidDays,
+            ranking: this.ranking,
+        };
+    }
+
+    addLog(log: RaidLogWithRealm): void {
+        const logId = log.log_id;
+        const bossName = log.encounter_data.encounter_name;
+        const raidName = log.mapentry.name;
+        const difficulty = log.difficulty;
+        const fightLength = log.fight_time;
+        const date = log.killtime;
+
+        const logDate = new Date(date * 1000);
+        const weekId: WeekId = id.weekId(time.getLatestWednesday(logDate));
+
+        const updateActivity = () => {
+            this.activity[difficulty] = date;
+        };
+
+        const updateRaidDays = () => {
+            this.raidDays.total[time.unshiftDateDay(logDate.getUTCDay())][
+                logDate.getUTCHours()
+            ] += 1;
+        };
+
+        const updateLatestKills = () => {
+            this.progression.latestKills.unshift({
+                id: logId,
+                date: date,
+                boss: bossName,
+                difficulty: difficulty,
+            });
+            this.progression.latestKills = this.progression.latestKills.slice(
+                0,
+                50
+            );
+        };
+
+        const updateRanking = () => {
+            const guildRankingFullClearCategory = [
+                raidName,
+                difficulty,
+                "fullClear",
+            ];
+
+            let guildRankingFullClear = (getNestedObjectValue(
+                this.ranking,
+                guildRankingFullClearCategory
+            ) as GuildRankingFull | undefined) || {
+                time: false,
+                logs: [],
+                weeks: {},
+            };
+
+            if (!guildRankingFullClear.weeks[weekId]) {
+                guildRankingFullClear.weeks[weekId] = [
+                    {
+                        members: log.members.map((member) => member.name),
+                        logs: [
+                            {
+                                bossName: bossName,
+                                date: date,
+                                fightLength: fightLength,
+                                id: logId,
+                            },
+                        ],
+                    },
+                ];
+            } else {
+                let logAddedToRanking = false;
+
+                for (let raidGroup of guildRankingFullClear.weeks[weekId]) {
+                    if (
+                        logHelper.sameMembers(
+                            raidGroup.members,
+                            log.members.map((member) => member.name),
+                            difficulty
+                        )
+                    ) {
+                        logAddedToRanking = true;
+                        raidGroup.logs.push({
+                            bossName: bossName,
+                            date: date,
+                            fightLength: fightLength,
+                            id: logId,
+                        });
+
+                        break;
+                    }
+                }
+
+                if (!logAddedToRanking) {
+                    guildRankingFullClear.weeks[weekId].push({
+                        members: log.members.map((member) => member.name),
+                        logs: [
+                            {
+                                bossName: bossName,
+                                date: date,
+                                fightLength: fightLength,
+                                id: logId,
+                            },
+                        ],
+                    });
+                }
+            }
+
+            this.ranking = addNestedObjectValue(
+                this.ranking,
+                guildRankingFullClearCategory,
+                guildRankingFullClear
+            );
+        };
+
+        const updateProgression = () => {
+            const guildBossCategorization = [
+                "raids",
+                raidName,
+                difficulty,
+                bossName,
+            ];
+
+            let currentGuildBoss =
+                (getNestedObjectValue(
+                    this.progression,
+                    guildBossCategorization
+                ) as GuildBoss | false) || this.createGuildBoss();
+
+            let guildKillLog: GuildKillLog = {
+                id: logId,
+                fightLength: fightLength,
+                date: date,
+            };
+
+            currentGuildBoss.killCount += 1;
+            currentGuildBoss.firstKills = currentGuildBoss.firstKills
+                .concat(guildKillLog)
+                .slice(0, 10);
+            currentGuildBoss.fastestKills = currentGuildBoss.fastestKills
+                .concat(guildKillLog)
+                .sort((a, b) => a.fightLength - b.fightLength)
+                .slice(0, 10);
+            currentGuildBoss.latestKills.unshift(guildKillLog);
+            currentGuildBoss.latestKills = currentGuildBoss.latestKills.slice(
+                0,
+                10
+            );
+
+            this.progression = addNestedObjectValue(
+                this.progression,
+                guildBossCategorization,
+                currentGuildBoss
+            ) as GuildProgression;
+        };
+
+        updateActivity();
+        updateRaidDays();
+        updateLatestKills();
+        updateRanking();
+        updateProgression();
+    }
+
+    private createGuildRaidDays(): GuildRaidDays {
+        return {
+            total: new Array(7).fill(0).map(() => new Array(24).fill(0)),
+            latest: new Array(7).fill(0).map(() => new Array(24).fill(0)),
+        };
+    }
+
+    private createGuildBoss(): GuildBoss {
+        return {
+            killCount: 0,
+            firstKills: [],
+            fastestKills: [],
+            latestKills: [],
+        };
+    }
+
+    isGuildDocument(obj: any): obj is GuildDocument {
+        if (obj && obj._id) {
+            return true;
+        }
+        return false;
+    }
+}
 
 export async function requestGuildDocument(guildName: string, realm: Realm) {
     const response = await tauriApi.getGuildData(guildName, realm);
@@ -66,209 +430,6 @@ export async function requestGuildDocument(guildName: string, realm: Realm) {
     }
 
     return newGuild;
-}
-
-export function createGuildDocument(
-    guildName: string,
-    realm: Realm,
-    faction: Faction
-): GuildDocument {
-    return {
-        _id: getGuildId(guildName, realm),
-        f: faction,
-        realm: realm,
-        name: guildName,
-        members: [],
-        ranks: [],
-        activity: {},
-        progression: {
-            latestKills: [],
-            completion: {
-                completed: false,
-                bossesDefeated: 0,
-                difficulties: {},
-            },
-            raids: {},
-        },
-        raidDays: createGuildRaidDays(),
-        ranking: {},
-    };
-}
-
-export function addLogToGuildDocument(
-    guild: GuildDocument,
-    log: RaidLogWithRealm,
-    raidName: RaidName,
-    logId: number,
-    bossName: string,
-    difficulty: Difficulty,
-    date: number,
-    fightLength: number
-): GuildDocument {
-    guild.activity[difficulty] = date;
-
-    const logDate = new Date(date * 1000);
-    guild.raidDays.total[timeTransformer.unshiftDateDay(logDate.getUTCDay())][
-        logDate.getUTCHours()
-    ] += 1;
-
-    guild.progression.latestKills.unshift({
-        id: logId,
-        date: date,
-        boss: bossName,
-        difficulty: difficulty,
-    });
-    guild.progression.latestKills = guild.progression.latestKills.slice(0, 50);
-
-    const guildRankingFullClearCategory = [raidName, difficulty, "fullClear"];
-
-    const weekId = timeTransformer.getLatestWednesday(logDate).getTime();
-
-    let guildRankingFullClear = getNestedObjectValue(
-        guild.ranking,
-        guildRankingFullClearCategory
-    ) as GuildRankingFull | false;
-
-    if (!guildRankingFullClear) {
-        guildRankingFullClear = {
-            time: false,
-            logs: [],
-            weeks: {},
-        };
-
-        guildRankingFullClear.weeks[weekId] = [
-            {
-                members: log.members.map((member) => member.name),
-                logs: [
-                    {
-                        bossName: bossName,
-                        date: date,
-                        fightLength: fightLength,
-                        id: logId,
-                    },
-                ],
-            },
-        ];
-    } else if (!guildRankingFullClear.weeks[weekId]) {
-        guildRankingFullClear.weeks[weekId] = [
-            {
-                members: log.members.map((member) => member.name),
-                logs: [
-                    {
-                        bossName: bossName,
-                        date: date,
-                        fightLength: fightLength,
-                        id: logId,
-                    },
-                ],
-            },
-        ];
-    } else {
-        let logAddedToRanking = false;
-
-        for (let i = 0; i < guildRankingFullClear.weeks[weekId].length; i++) {
-            let raidGroup = guildRankingFullClear.weeks[weekId][i];
-
-            if (
-                logs.sameMembers(
-                    raidGroup.members,
-                    log.members.map((member) => member.name),
-                    difficulty
-                )
-            ) {
-                logAddedToRanking = true;
-
-                raidGroup.logs.push({
-                    bossName: bossName,
-                    date: date,
-                    fightLength: fightLength,
-                    id: logId,
-                });
-
-                guildRankingFullClear.weeks[weekId][i] = raidGroup;
-
-                break;
-            }
-        }
-
-        if (!logAddedToRanking) {
-            guildRankingFullClear.weeks[weekId].push({
-                members: log.members.map((member) => member.name),
-                logs: [
-                    {
-                        bossName: bossName,
-                        date: date,
-                        fightLength: fightLength,
-                        id: logId,
-                    },
-                ],
-            });
-        }
-    }
-
-    guild.ranking = addNestedObjectValue(
-        guild.ranking,
-        guildRankingFullClearCategory,
-        guildRankingFullClear
-    );
-
-    const guildBossCategorization = [
-        "progression",
-        "raids",
-        raidName,
-        difficulty,
-        bossName,
-    ];
-
-    let currentGuildBoss = getNestedObjectValue(
-        guild,
-        guildBossCategorization
-    ) as GuildBoss | false;
-
-    if (!currentGuildBoss) {
-        currentGuildBoss = createGuildBoss();
-    }
-
-    let guildKillLog: GuildKillLog = {
-        id: logId,
-        fightLength: fightLength,
-        date: date,
-    };
-
-    currentGuildBoss.killCount += 1;
-    currentGuildBoss.firstKills = currentGuildBoss.firstKills
-        .concat(guildKillLog)
-        .slice(0, 10);
-    currentGuildBoss.fastestKills = currentGuildBoss.fastestKills
-        .concat(guildKillLog)
-        .sort((a, b) => a.fightLength - b.fightLength)
-        .slice(0, 10);
-    currentGuildBoss.latestKills.unshift(guildKillLog);
-    currentGuildBoss.latestKills = currentGuildBoss.latestKills.slice(0, 10);
-
-    guild = addNestedObjectValue(
-        guild,
-        guildBossCategorization,
-        currentGuildBoss
-    ) as GuildDocument;
-
-    return guild;
-}
-
-export function createGuildRaidDays(): GuildRaidDays {
-    return {
-        total: new Array(7).fill(0).map(() => new Array(24).fill(0)),
-        latest: new Array(7).fill(0).map(() => new Array(24).fill(0)),
-    };
-}
-
-export function createGuildBoss(): GuildBoss {
-    return {
-        killCount: 0,
-        firstKills: [],
-        fastestKills: [],
-        latestKills: [],
-    };
 }
 
 export function updateGuildDocument(
@@ -457,7 +618,7 @@ export function getLatestGuildRaidDays(logs: GuildLatestKill[]) {
         JSON.stringify(createGuildRaidDays())
     );
 
-    const timeBoundary = timeTransformer
+    const timeBoundary = time
         .getLatestWednesday(new Date(new Date().getTime() - week * 2))
         .getTime();
 
@@ -465,7 +626,7 @@ export function getLatestGuildRaidDays(logs: GuildLatestKill[]) {
         if (log.date * 1000 > timeBoundary) {
             let logDate = new Date(log.date * 1000);
 
-            raidDays[timeTransformer.unshiftDateDay(logDate.getUTCDay())][
+            raidDays[time.unshiftDateDay(logDate.getUTCDay())][
                 logDate.getUTCHours()
             ] += 1;
         } else {
