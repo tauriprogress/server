bd6429f3db2e63a03b6aa2955baa7940cc441541
akoskoc <akoskoc@gmail.com>
2023-09-09 08:51:52 +0200

    document manager class

--- src/helpers/documents/index.ts
+++ src/helpers/documents/index.ts
@@ -2,3 +2,18 @@ export * from "./character";
 export * from "./raidBoss";
 export * from "./guild";
 export * from "./maintenance";
+import { characterDocument } from "./character";
+import { GuildDocumentController } from "./guild";
+import RaidBossDocumentController from "./raidBoss";
+import WeeklyFullClearDocumentController from "./weeklyFullClear";
+
+class DocumentManager {
+    weeklyFullClear = WeeklyFullClearDocumentController;
+    raidBoss = RaidBossDocumentController;
+    guild = GuildDocumentController;
+    character = characterDocument;
+}
+
+const documentManager = new DocumentManager();
+
+export default documentManager;
