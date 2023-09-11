902ac0f219c3be99f00d93a8e03fd43811980851
akoskoc <akoskoc@gmail.com>
2023-09-11 08:42:58 +0200

    validator class

--- src/helpers/validators.ts
+++ src/helpers/validators.ts
@@ -1,296 +1,254 @@
 import environment from "../environment";
-import {
-    Difficulty,
-    LastRaidLogWithRealm,
-    RaidLog,
-    Realm,
-    ShortRealm,
-    ValidMember,
-} from "../types";
+import { Difficulty, LastRaidLogWithRealm, Realm, ShortRealm } from "../types";
 
-export function validRaidId(raidId: any) {
-    if (typeof raidId === "number") {
-        for (const raid of environment.currentContent.raids) {
-            if (raid.id === raidId) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-export function validShortRealm(shortRealm: any) {
-    return (
-        typeof shortRealm === "string" &&
-        Object.values(environment.shortRealms).includes(
-            shortRealm as ShortRealm
-        )
-    );
-}
-
-export function validRealm(realm: any) {
-    return (
-        typeof realm === "string" &&
-        Object.values(environment.realms).includes(realm as Realm)
-    );
-}
-
-export function validClass(characterClass: any) {
-    return (
-        typeof characterClass === "number" &&
-        environment.characterClassNames.hasOwnProperty(characterClass)
-    );
-}
-
-export function validSpec(characterSpec: any) {
-    return (
-        typeof characterSpec === "number" &&
-        environment.specs.hasOwnProperty(characterSpec)
-    );
-}
-
-export function validRole(role: any) {
-    return (
-        typeof role === "string" &&
-        (role === "damage" || role === "heal" || role === "tank")
-    );
-}
-
-export function validFaction(faction: any) {
-    return typeof faction === "number" && (faction === 0 || faction === 1);
-}
-
-export function validRaidName(raidName: any) {
-    if (typeof raidName === "string") {
-        for (const raid of environment.currentContent.raids) {
-            if (raid.name === raidName) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-export function validBossName(raidId: any, bossName: any) {
-    if (typeof raidId === "number" && typeof bossName === "string") {
-        for (const raid of environment.currentContent.raids) {
-            if (raid.id === raidId) {
-                for (const boss of raid.bosses) {
-                    if (boss.name === bossName) {
-                        return true;
-                    }
-                }
-            }
-        }
-    }
-
-    return false;
-}
-
-export function validIngameBossId(
-    ingameBossId: number,
-    difficulty: Difficulty
-): boolean {
-    if (typeof ingameBossId !== "number" || typeof difficulty !== "number")
-        return false;
-
-    for (const raid of environment.currentContent.raids) {
-        for (const boss of raid.bosses) {
-            const diff = difficulty as keyof typeof boss.bossIdOfDifficulty;
-            if (!boss.bossIdOfDifficulty[diff]) continue;
-            if (boss.bossIdOfDifficulty[diff] === ingameBossId) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-export function validDifficulty(raidId: any, difficulty: any) {
-    if (typeof raidId === "number" && typeof difficulty === "number") {
-        for (const raid of environment.currentContent.raids) {
-            if (raid.id === raidId) {
-                for (const raidDifficulty of raid.difficulties) {
-                    if (difficulty === raidDifficulty) {
-                        return true;
-                    }
-                }
-            }
-        }
-    }
-
-    return false;
-}
-
-export function sameMembers(
-    members1: string[],
-    members2: string[],
-    difficulty: 10 | 25
-): boolean {
-    let memberContainer: { [propName: string]: boolean } = {};
-    let sameMemberCount = 1;
-
-    for (let name of members1) {
-        memberContainer[name] = true;
-    }
-
-    for (let name of members2) {
-        if (memberContainer[name]) {
-            sameMemberCount++;
-        }
-    }
-
-    return difficulty * 0.8 <= sameMemberCount;
-}
-
-export function isSeasonRunning() {
-    return !!getCurrentSeason();
-}
-
-export function getCurrentSeason() {
-    const currentDate = new Date().getTime();
-
-    if (environment.seasonal) {
-        for (const season of environment.seasons) {
-            const start = new Date(season.start).getTime();
-            const finish = new Date(season.finish).getTime();
-            if (currentDate > start && currentDate < finish) {
-                return season;
-            }
-        }
-    }
-
-    return false;
-}
-
-export function validLogDate(date: Date) {
-    if (!environment.seasonal) {
-        return true;
-    }
-
-    const currentSeason = getCurrentSeason();
-
-    if (currentSeason) {
-        const time = date.getTime();
-
-        const start = new Date(currentSeason.start).getTime();
-        const finish = new Date(currentSeason.finish).getTime();
-
-        if (time > start && time < finish) {
+class Validator {
+    validLog(log: LastRaidLogWithRealm) {
+        if (
+            this.validLogDate(new Date(log.killtime * 1000)) &&
+            this.validRaidName(log.mapentry.name) &&
+            this.validDifficulty(log.mapentry.id, log.difficulty) &&
+            this.validBossName(
+                log.mapentry.id,
+                log.encounter_data.encounter_name
+            ) &&
+            log.fight_time > 10000
+        ) {
             return true;
         }
+        return false;
     }
 
-    return false;
-}
+    validLogDate(date: Date) {
+        if (!environment.seasonal) {
+            return true;
+        }
 
-export function validCombatMetric(combatMetric: any) {
-    return (
-        typeof combatMetric === "string" &&
-        (combatMetric === "dps" || combatMetric === "hps")
-    );
-}
+        const currentSeason = environment.getCurrentSeason();
 
-export function validFilters(raidId: number, filters: any) {
-    if (
-        typeof filters === "object" &&
-        filters !== null &&
-        !Array.isArray(filters)
-    ) {
+        if (currentSeason) {
+            const time = date.getTime();
+
+            const start = new Date(currentSeason.start).getTime();
+            const finish = new Date(currentSeason.finish).getTime();
+
+            if (time > start && time < finish) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    validRaidId(raidId: any) {
+        if (typeof raidId === "number") {
+            for (const raid of environment.currentContent.raids) {
+                if (raid.id === raidId) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    validShortRealm(shortRealm: any) {
+        return (
+            typeof shortRealm === "string" &&
+            Object.values(environment.shortRealms).includes(
+                shortRealm as ShortRealm
+            )
+        );
+    }
+
+    validRealm(realm: any) {
+        return (
+            typeof realm === "string" &&
+            Object.values(environment.realms).includes(realm as Realm)
+        );
+    }
+
+    validClass(characterClass: any) {
+        return (
+            typeof characterClass === "number" &&
+            environment.characterClassNames.hasOwnProperty(characterClass)
+        );
+    }
+
+    validSpec(characterSpec: any) {
+        return (
+            typeof characterSpec === "number" &&
+            environment.specs.hasOwnProperty(characterSpec)
+        );
+    }
+
+    validRole(role: any) {
+        return (
+            typeof role === "string" &&
+            (role === "damage" || role === "heal" || role === "tank")
+        );
+    }
+
+    validFaction(faction: any) {
+        return typeof faction === "number" && (faction === 0 || faction === 1);
+    }
+
+    validRaidName(raidName: any) {
+        if (typeof raidName === "string") {
+            for (const raid of environment.currentContent.raids) {
+                if (raid.name === raidName) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    validBossName(raidId: any, bossName: any) {
+        if (typeof raidId === "number" && typeof bossName === "string") {
+            for (const raid of environment.currentContent.raids) {
+                if (raid.id === raidId) {
+                    for (const boss of raid.bosses) {
+                        if (boss.name === bossName) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    validIngameBossId(ingameBossId: number, difficulty: Difficulty): boolean {
+        if (typeof ingameBossId !== "number" || typeof difficulty !== "number")
+            return false;
+
+        for (const raid of environment.currentContent.raids) {
+            for (const boss of raid.bosses) {
+                const diff = difficulty as keyof typeof boss.bossIdOfDifficulty;
+                if (!boss.bossIdOfDifficulty[diff]) continue;
+                if (boss.bossIdOfDifficulty[diff] === ingameBossId) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    validDifficulty(raidId: any, difficulty: any) {
+        if (typeof raidId === "number" && typeof difficulty === "number") {
+            for (const raid of environment.currentContent.raids) {
+                if (raid.id === raidId) {
+                    for (const raidDifficulty of raid.difficulties) {
+                        if (difficulty === raidDifficulty) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    validCombatMetric(combatMetric: any) {
+        return (
+            typeof combatMetric === "string" &&
+            (combatMetric === "dps" || combatMetric === "hps")
+        );
+    }
+
+    validFilters(raidId: number, filters: any) {
         if (
-            filters.difficulty === undefined ||
-            !validDifficulty(raidId, filters.difficulty)
+            typeof filters === "object" &&
+            filters !== null &&
+            !Array.isArray(filters)
         ) {
-            return false;
-        }
-
-        if (filters.realm !== undefined && !validRealm(filters.realm)) {
-            return false;
-        }
-
-        if (filters.class !== undefined && !validClass(filters.class)) {
-            return false;
-        }
-
-        if (filters.spec !== undefined && !validSpec(filters.spec)) {
-            return false;
-        }
-
-        if (filters.role !== undefined && !validRole(filters.role)) {
-            return false;
-        }
-
-        if (filters.faction !== undefined && !validFaction(filters.faction)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    return false;
-}
-
-export function validPage(page: any) {
-    return typeof page === "number" && page >= 0;
-}
-
-export function validPageSize(pageSize: any) {
-    return typeof pageSize === "number" && pageSize >= 1;
-}
-
-export function validGuildName(guildName: any) {
-    return typeof guildName === "string";
-}
-
-export function validCharacterName(characterName: any) {
-    return typeof characterName === "string";
-}
-
-export function validLogId(logId: any) {
-    return typeof logId === "number";
-}
-
-export function validLimit(limit: any) {
-    return typeof limit === "number";
-}
-
-export function validItems(items: any) {
-    return (
-        Array.isArray(items) &&
-        items.reduce((acc: boolean, curr: any) => {
-            if (typeof curr.id !== "number") {
+            if (
+                filters.difficulty === undefined ||
+                !this.validDifficulty(raidId, filters.difficulty)
+            ) {
                 return false;
             }
 
-            return acc;
-        }, true)
-    );
-}
+            if (
+                filters.realm !== undefined &&
+                !this.validRealm(filters.realm)
+            ) {
+                return false;
+            }
 
-export function isError(variable: unknown): variable is Error {
-    return variable instanceof Error;
-}
+            if (
+                filters.class !== undefined &&
+                !this.validClass(filters.class)
+            ) {
+                return false;
+            }
 
-export function validRaidLog(log: LastRaidLogWithRealm) {
-    if (
-        validLogDate(new Date(log.killtime * 1000)) &&
-        validRaidName(log.mapentry.name) &&
-        validDifficulty(log.mapentry.id, log.difficulty) &&
-        validBossName(log.mapentry.id, log.encounter_data.encounter_name) &&
-        log.fight_time > 10000
-    ) {
-        return true;
+            if (filters.spec !== undefined && !this.validSpec(filters.spec)) {
+                return false;
+            }
+
+            if (filters.role !== undefined && !this.validRole(filters.role)) {
+                return false;
+            }
+
+            if (
+                filters.faction !== undefined &&
+                !this.validFaction(filters.faction)
+            ) {
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
     }
-    return false;
+
+    validPage(page: any) {
+        return typeof page === "number" && page >= 0;
+    }
+
+    validPageSize(pageSize: any) {
+        return typeof pageSize === "number" && pageSize >= 1;
+    }
+
+    validGuildName(guildName: any) {
+        return typeof guildName === "string";
+    }
+
+    validCharacterName(characterName: any) {
+        return typeof characterName === "string";
+    }
+
+    validLogId(logId: any) {
+        return typeof logId === "number";
+    }
+
+    validLimit(limit: any) {
+        return typeof limit === "number";
+    }
+
+    validItems(items: any) {
+        return (
+            Array.isArray(items) &&
+            items.reduce((acc: boolean, curr: any) => {
+                if (typeof curr.id !== "number") {
+                    return false;
+                }
+
+                return acc;
+            }, true)
+        );
+    }
+
+    isError(variable: unknown): variable is Error {
+        return variable instanceof Error;
+    }
 }
 
-export function validMember(
-    member: RaidLog["members"][number]
-): member is ValidMember {
-    if (member.spec) {
-        return true;
-    }
-    return false;
-}
+export const validator = new Validator();
+
+export default validator;
