65af41c183e3ce9436e6127077d81cae05659204
akoskoc <akoskoc@gmail.com>
2023-09-20 14:03:40 +0200

    remove unused files

--- src/types/documents/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./character";
-export * from "./raidBoss";
-export * from "./guild";
-export * from "./maintenance";
-export * from "./leaderboardCharacter";
--- src/types/logs/characterPerformanceOfBoss.ts
+++
@@ -1,9 +0,0 @@
-import { CharacterDocument, CombatMetric } from "..";
-
-export type CharacterPerformanceOfBoss = {
-    [bossId: string]: {
-        [K in CombatMetric]: {
-            [characterId: string]: CharacterDocument;
-        };
-    };
-};
