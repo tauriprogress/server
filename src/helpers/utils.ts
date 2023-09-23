dbd623ed5334a376495f445d2fae1cc932223ebb
akoskoc <akoskoc@gmail.com>
2023-09-23 18:51:07 +0200

    refactor util functions

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
+import { LooseObject } from "../types";
 
 export function runGC() {
     try {
@@ -26,5 +27,56 @@ export function ensureFile(filePath: string) {
             fs.mkdirSync(dirname);
         }
         fs.closeSync(fs.openSync(filePath, "w"));
+    }
+}
+
+export function capitalize<T extends string>(str: T): Capitalize<T> {
+    const capitalized = (str.charAt(0).toUpperCase() +
+        str.slice(1).toLowerCase()) as Capitalize<T>;
+
+    return capitalized.length === str.length
+        ? capitalized
+        : (str as Capitalize<T>);
+}
+
+export function getRelativePerformance(
+    currentPerformance: number,
+    bestPerformance: number
+) {
+    if (currentPerformance === 0 || bestPerformance === 0) return 0;
+    return Math.round((currentPerformance / bestPerformance) * 1000) / 10;
+}
+
+export function addNestedObjectValue<T>(
+    obj: LooseObject,
+    keys: Array<string | number>,
+    value: T
+) {
+    let currentKey = keys[0];
+
+    if (currentKey !== undefined) {
+        obj[currentKey] = addNestedObjectValue(
+            obj.hasOwnProperty(currentKey) ? obj[currentKey] : {},
+            keys.slice(1, keys.length),
+            value
+        );
+        return obj;
+    } else {
+        return value !== undefined ? value : {};
+    }
+}
+
+export function getNestedObjectValue(
+    obj: LooseObject,
+    keys: Array<string | number>
+): any {
+    let currentKey = keys[0];
+
+    if (keys.length === 1) {
+        return obj[currentKey];
+    } else {
+        return obj.hasOwnProperty(currentKey)
+            ? getNestedObjectValue(obj[currentKey], keys.slice(1, keys.length))
+            : undefined;
     }
 }
