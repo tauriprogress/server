c3e8774e2815ba603eef60fc042de07984b747b4
akoskoc <akoskoc@gmail.com>
2023-10-27 00:43:18 +0200

    refactor time helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,7 @@ export * from "./documents";
 export * from "./id";
 export * from "./lock";
 export * from "./log";
-export * from "./time";
 export * from "./utils";
+export { default as time } from "./time";
 export { default as validator } from "./validators";
 export { default as log } from "./log";
--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,46 +1,47 @@
 import { Second } from "../types";
 
-class Time {
-    getLatestWednesday(currentDate: Date = new Date()) {
-        const currentDay = currentDate.getUTCDay();
+function getLatestWednesday(currentDate: Date = new Date()) {
+    const currentDay = currentDate.getUTCDay();
 
-        const wednesdayDaysAgo =
-            (currentDay < 3 ? currentDay + 7 : currentDay) - 3;
+    const wednesdayDaysAgo = (currentDay < 3 ? currentDay + 7 : currentDay) - 3;
 
-        let lastWednesdayDate = currentDate.getUTCDate() - wednesdayDaysAgo;
+    let lastWednesdayDate = currentDate.getUTCDate() - wednesdayDaysAgo;
 
-        if (currentDay === 3 && currentDate.getUTCHours() < 7) {
-            lastWednesdayDate -= 7;
-        }
-
-        return new Date(
-            Date.UTC(
-                currentDate.getUTCFullYear(),
-                currentDate.getUTCMonth(),
-                lastWednesdayDate,
-                7
-            )
-        );
+    if (currentDay === 3 && currentDate.getUTCHours() < 7) {
+        lastWednesdayDate -= 7;
     }
 
-    unshiftDateDay(day: number) {
-        // date.getDay returns a number that represents the day, 0 by deafult is sunday, instead i prefer 0 to be monday, thats the reason for this function
-        return day - 1 >= 0 ? day - 1 : 6;
-    }
-
-    minutesAgo(seconds: number) {
-        return Math.round((new Date().getTime() / 1000 - Number(seconds)) / 60);
-    }
-
-    getCurrentDateInSeconds(): Second {
-        return new Date().getTime() / 1000;
-    }
-
-    dateToString(date: Date): string {
-        return date.getTime().toString();
-    }
+    return new Date(
+        Date.UTC(
+            currentDate.getUTCFullYear(),
+            currentDate.getUTCMonth(),
+            lastWednesdayDate,
+            7
+        )
+    );
 }
 
-export const time = new Time();
+function unshiftDateDay(day: number) {
+    // date.getDay returns a number that represents the day, 0 by deafult is sunday, instead i prefer 0 to be monday, thats the reason for this function
+    return day - 1 >= 0 ? day - 1 : 6;
+}
 
-export default time;
+function minutesAgo(seconds: number) {
+    return Math.round((new Date().getTime() / 1000 - Number(seconds)) / 60);
+}
+
+function getCurrentDateInSeconds(): Second {
+    return new Date().getTime() / 1000;
+}
+
+function dateToString(date: Date): string {
+    return date.getTime().toString();
+}
+
+export default {
+    getLatestWednesday,
+    unshiftDateDay,
+    minutesAgo,
+    getCurrentDateInSeconds,
+    dateToString,
+};
