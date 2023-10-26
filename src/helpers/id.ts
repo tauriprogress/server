f5958e92d807f719f4ebb5eff3ef23dce731abcf
akoskoc <akoskoc@gmail.com>
2023-10-27 00:54:53 +0200

    refactor id helpers

--- src/helpers/id.ts
+++ src/helpers/id.ts
@@ -16,106 +16,100 @@ export type RaidBossId = string;
 export type CharacterId = string;
 export type CharacterDocumentCollectionId = string;
 
-class Id {
-    deconstruct: DeconstructId;
-    cache: CacheId;
-
-    constructor() {
-        this.deconstruct = new DeconstructId();
-        this.cache = new CacheId();
-    }
-
-    guildId(guildName: string, guildRealm: Realm): GuildId {
-        return `${guildName} ${guildRealm}`;
-    }
-
-    raidBossId(ingameBossId: number, difficulty: Difficulty): RaidBossId {
-        return `${ingameBossId} ${difficulty}`;
-    }
-
-    characterId(name: string, realm: Realm, spec: SpecId): CharacterId {
-        return `${name},${environment.shortRealms[realm]},${spec}`;
-    }
-
-    characterDocumentCollectionId(
-        ingameBossId: number,
-        difficulty: Difficulty,
-        combatMetric: CombatMetric
-    ): CharacterDocumentCollectionId {
-        return `${ingameBossId} ${difficulty} ${combatMetric}`;
-    }
-
-    leaderboardCharacterId(
-        name: string,
-        classId: ClassId,
-        realm: Realm,
-        raidName: RaidName,
-        difficulty: Difficulty
-    ) {
-        return `${name},${classId},${environment.shortRealms[realm]},${raidName},${difficulty}`;
-    }
+function guildId(guildName: string, guildRealm: Realm): GuildId {
+    return `${guildName} ${guildRealm}`;
 }
 
-class DeconstructId {
-    characterDocumentCollectionId(
-        characterDocumentCollectionId: ReturnType<
-            typeof id.characterDocumentCollectionId
-        >
-    ) {
-        const [ingameBossId, difficulty, combatMetric] =
-            characterDocumentCollectionId.split(" ");
-
-        return [
-            Number(ingameBossId),
-            Number(difficulty) as Difficulty,
-            combatMetric as CombatMetric,
-        ] as const;
-    }
-
-    raidBossId(bossId: ReturnType<typeof id.raidBossId>) {
-        const [ingameBossId, difficulty] = bossId.split(" ");
-
-        return [
-            Number(ingameBossId),
-            Number(difficulty) as Difficulty,
-        ] as const;
-    }
+function raidBossId(ingameBossId: number, difficulty: Difficulty): RaidBossId {
+    return `${ingameBossId} ${difficulty}`;
 }
 
-class CacheId {
-    raidSummaryCacheId(raidId: RaidId) {
-        return `${raidId}`;
-    }
-    raidBossCacheId(raidId: number, bossName: string) {
-        return `${raidId}${bossName}`;
-    }
-
-    characterPerformanceCacheId(
-        characterName: string,
-        realm: Realm,
-        raidName: RaidName
-    ) {
-        return `${characterName}${realm}${raidName}`;
-    }
-
-    characterLeaderboardCacheId(
-        raidName: RaidName,
-        combatMetric: CombatMetric,
-        filters: Filters,
-        page: number
-    ) {
-        return `${raidName}${combatMetric}${filters.difficulty}${filters.faction}${filters.class}${filters.realm}${page}`;
-    }
-
-    extendedLogId(logId: number, realm: Realm) {
-        return `${logId}${realm}`;
-    }
-
-    characterApiId(characterName: string, realm: Realm) {
-        return capitalize(`${characterName}${realm}`);
-    }
+function characterId(name: string, realm: Realm, spec: SpecId): CharacterId {
+    return `${name},${environment.shortRealms[realm]},${spec}`;
 }
 
-export const id = new Id();
+function characterDocumentCollectionId(
+    ingameBossId: number,
+    difficulty: Difficulty,
+    combatMetric: CombatMetric
+): CharacterDocumentCollectionId {
+    return `${ingameBossId} ${difficulty} ${combatMetric}`;
+}
 
-export default id;
+function leaderboardCharacterId(
+    name: string,
+    classId: ClassId,
+    realm: Realm,
+    raidName: RaidName,
+    difficulty: Difficulty
+) {
+    return `${name},${classId},${environment.shortRealms[realm]},${raidName},${difficulty}`;
+}
+function deconstructCharacterDocumentCollectionId(
+    collectionId: ReturnType<typeof characterDocumentCollectionId>
+) {
+    const [ingameBossId, difficulty, combatMetric] = collectionId.split(" ");
+
+    return [
+        Number(ingameBossId),
+        Number(difficulty) as Difficulty,
+        combatMetric as CombatMetric,
+    ] as const;
+}
+function deconstructRaidBossId(bossId: ReturnType<typeof raidBossId>) {
+    const [ingameBossId, difficulty] = bossId.split(" ");
+
+    return [Number(ingameBossId), Number(difficulty) as Difficulty] as const;
+}
+
+function raidSummaryCacheId(raidId: RaidId) {
+    return `${raidId}`;
+}
+function raidBossCacheId(raidId: number, bossName: string) {
+    return `${raidId}${bossName}`;
+}
+
+function characterPerformanceCacheId(
+    characterName: string,
+    realm: Realm,
+    raidName: RaidName
+) {
+    return `${characterName}${realm}${raidName}`;
+}
+
+function characterLeaderboardCacheId(
+    raidName: RaidName,
+    combatMetric: CombatMetric,
+    filters: Filters,
+    page: number
+) {
+    return `${raidName}${combatMetric}${filters.difficulty}${filters.faction}${filters.class}${filters.realm}${page}`;
+}
+
+function extendedLogId(logId: number, realm: Realm) {
+    return `${logId}${realm}`;
+}
+
+function characterApiId(characterName: string, realm: Realm) {
+    return capitalize(`${characterName}${realm}`);
+}
+
+export default {
+    guildId,
+    raidBossId,
+    characterId,
+    characterDocumentCollectionId,
+    leaderboardCharacterId,
+    deconstruct: {
+        characterDocumentCollectionId: deconstructCharacterDocumentCollectionId,
+        raidBossId: deconstructRaidBossId,
+    },
+    cache: {
+        raidSummaryCacheId,
+        raidBossCacheId,
+        characterPerformanceCacheId,
+        characterLeaderboardCacheId,
+        extendedLogId,
+        characterApiId,
+    },
+};
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,9 @@
 export * from "./documents";
-export * from "./id";
 export * from "./lock";
-export * from "./log";
 export * from "./utils";
+export * from "./id";
+export { default as id } from "./id";
+export * from "./log";
+export { default as log } from "./log";
 export { default as time } from "./time";
 export { default as validator } from "./validators";
-export { default as log } from "./log";
