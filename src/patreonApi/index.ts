55c84f4a100934d8779b42d6fb7f8dc3f64b50a2
akoskoc <akoskoc@gmail.com>
2024-01-08 11:58:18 +0100

    refactor patreon api to use errors + refresh token

--- src/patreonApi/index.ts
+++ src/patreonApi/index.ts
@@ -1,17 +1,48 @@
 import environment from "../environment";
-import { GetAuthTokenResponse, GetUserInfoResponse } from "../types";
+import {
+    GetUserInfoResponse,
+    PatreonAuthResponse,
+    PatreonError,
+} from "../types";
 
 class PatreonApi {
     private baseUrl = "https://www.patreon.com/api/oauth2";
-    getAuthToken(code: string): Promise<GetAuthTokenResponse> {
+    getAuthToken(code: string): Promise<PatreonAuthResponse> {
         return new Promise(async (resolve, reject) => {
             try {
-                const response: GetAuthTokenResponse = await fetch(
-                    `${this.baseUrl}/token?code=${code}&grant_type=authorization_code&client_id=${environment.PATREON_CLIENT}&client_secret=${environment.PATREON_SECRET}&redirect_uri=${environment.CORS_ORIGIN}/${environment.REALM_GROUP}/login`,
-                    {
-                        method: "POST",
-                    }
-                ).then((res) => res.json());
+                const response: PatreonAuthResponse | PatreonError =
+                    await fetch(
+                        `${this.baseUrl}/token?code=${code}&grant_type=authorization_code&client_id=${environment.PATREON_CLIENT}&client_secret=${environment.PATREON_SECRET}&redirect_uri=${environment.CORS_ORIGIN}/${environment.REALM_GROUP}/login`,
+                        {
+                            method: "POST",
+                        }
+                    ).then((res) => res.json());
+
+                if ("error" in response) {
+                    throw new Error(response.error);
+                }
+
+                resolve(response);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    refreshToken(token: string): Promise<PatreonAuthResponse> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response: PatreonAuthResponse | PatreonError =
+                    await fetch(
+                        `${this.baseUrl}/token?grant_type=refresh_token&refresh_token=${token}&client_id=${environment.PATREON_CLIENT}&client_secret=${environment.PATREON_SECRET}&redirect_uri=${environment.CORS_ORIGIN}/${environment.REALM_GROUP}/login`,
+                        {
+                            method: "POST",
+                        }
+                    ).then((res) => res.json());
+
+                if ("error" in response) {
+                    throw new Error(response.error);
+                }
 
                 resolve(response);
             } catch (err) {
@@ -23,17 +54,22 @@ class PatreonApi {
     getUserInfo(authToken: string): Promise<GetUserInfoResponse> {
         return new Promise(async (resolve, reject) => {
             try {
-                const response: GetUserInfoResponse = await fetch(
-                    encodeURI(
-                        `${this.baseUrl}/v2/identity?include=memberships`
-                    ),
-                    {
-                        method: "GET",
-                        headers: {
-                            Authorization: `Bearer ${authToken}`,
-                        },
-                    }
-                ).then((res) => res.json());
+                const response: GetUserInfoResponse | PatreonError =
+                    await fetch(
+                        encodeURI(
+                            `${this.baseUrl}/v2/identity?include=memberships`
+                        ),
+                        {
+                            method: "GET",
+                            headers: {
+                                Authorization: `Bearer ${authToken}`,
+                            },
+                        }
+                    ).then((res) => res.json());
+
+                if ("error" in response) {
+                    throw new Error(response.error);
+                }
 
                 resolve(response);
             } catch (err) {
